7f5d7875c52174b76bb717ab698d738835105c87
einsvein <36289813+einsvein@users.noreply.github.com>
2025-03-02 13:50:51 +0100

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<nav>
+        <a href="index.php">Home</a>
+        <a href="start.php">Start Case</a>
+        <!-- <a href="dynamic_form.php' . ($case_id ? '?case_id=' . $case_id : '') . '">Dynamic Form</a>
+        <a href="double_tax_treaty.php' . ($case_id ? '?case_id=' . $case_id : '') . '">Double Tax Treaty</a>
+        <a href="case_summary.php' . ($case_id ? '?case_id=' . $case_id : '') . '">Case Summary</a> -->
+    </nav>
